331537d10b64c16bb9cdc9d49b941fb53e2ac831
wisnudj <jayalahwisnu@gmail.com>
2017-11-24 13:42:23 +0700

    update index

--- client/assets/js/comic-handler.js
+++ client/assets/js/comic-handler.js
@@ -1,15 +1,36 @@
 new Vue({
   el: '#app',
   data: {
-    link: '#'
+    link: '#',
+    comics: []
   },
   methods: {
     downloadlink: function (payload) {
       this.link = payload.link
+    },
+
+    readDB: function () {
+      axios.get('http://localhost:3000/allcomic')
+        .then((response) => {
+          this.comics = response.data
+          console.log(this.comics);
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+    },
+
+    sendid: function(id) {
+      localStorage.setItem("idcomic", id)
+      console.log(localStorage.getItem("idcomic"))
     }
   },
   ready() {
     console.log('-----------mounted')
     this.downloadlink();
+  },
+
+  created() {
+    this.readDB();
   }
 })
